a51292c1eafafbf06576d6e58bad15e34aa9680e
JosefLitos <litosjos@fit.cvut.cz>
2023-07-22 20:11:12 +0200

    chore(bash): proper indent aligner + opt highlight

--- src/bash.c
+++ src/bash.c
@@ -1,38 +1,39 @@
 #include "utils.h"
 
 char* bash_fmt(const char* doc, char* fmt, int len) {
-	if (alike(doc, "``` man\n") <= 0) { // don't format if not in the expected form of bashls
-		while (*doc) *fmt++ = *doc++;
-		return fmt;
-	} else {
-		doc += 6;
+	if (alike(doc, "``` man\n") > 0) { // don't format if not in the expected form of bashls
 		while (doc[len] != '`') len--;
+		doc += 6;
 		len -= 4;
 	}
 	const char* docEnd = doc + len;
 
-	int indent[]       = {8, 0, 0, 0}; // text indent, 1st lvl, 2nd lvl, 1st lvl set text wrap indent
-	char separated     = 0;
-	char kind          = 0;
+	int indent[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, lvl = 0;
+	char separated = 0;
+	char optlvl    = 0;
 	while (++doc < docEnd) {
 		switch (*doc) {
 			case '"':
-				*fmt++ = '*';
-				*fmt++ = '"';
-				while (*++doc && *doc != '"') *fmt++ = *doc;
-				*fmt++ = '"';
-				*fmt++ = '*';
+				if (('A' <= doc[1] && doc[1] <= 'Z') || ('a' <= doc[1] && doc[1] <= 'z')) {
+					char x = *doc;
+					*fmt++ = '*';
+					*fmt++ = x;
+					while (*++doc && *doc != x) *fmt++ = *doc;
+					*fmt++ = x;
+					*fmt++ = '*';
+				} else *fmt++ = *doc;
 				break;
 			case '<':
-				*fmt++ = '*';
-				*fmt++ = '<';
-				while (*++doc > ' ' && *doc != '>') *fmt++ = *doc;
-				if (*doc == '>') *fmt++ = '>';
-				else doc--;
-				*fmt++ = '*';
+				if (('A' <= doc[1] && doc[1] <= 'Z') || ('a' <= doc[1] && doc[1] <= 'z')) {
+					*fmt++ = '*';
+					*fmt++ = '<';
+					while (*++doc && *doc != '>') *fmt++ = *doc;
+					*fmt++ = '>';
+					*fmt++ = '*';
+				} else *fmt++ = '<';
 				break;
 			case '-':
-				if (fmt[-1] < '0' && (doc[1] == '-' || ('a' <= doc[1] && doc[1] <= 'z')) && doc[2] != '-') {
+				if (fmt[-1] < '0' && 'a' <= doc[doc[1] == '-' ? 2 : 1]) {
 					*fmt++ = '`';
 					while (*doc >= '0' || *doc == '-') *fmt++ = *doc++;
 					*fmt++ = '`';
@@ -52,30 +53,62 @@ char* bash_fmt(const char* doc, char* fmt, int len) {
 				if (doc[1] == '\n') {
 					separated = 1;
 				} else if (doc[1] == ' ') {
-					if (separated == -1) *fmt++ = '\n';
-					else if (separated == 1) {
-						if (kind == '-') fmt--;
-						kind = 0;
-					}
 					int i = 0;
 					while (*++doc == ' ') i++;
-					if (i >= 7) {
+					int ilvl = lvl;
+					if (i < indent[lvl] - 3)
+						while (ilvl && i < indent[ilvl] - 3) ilvl--;
+					if (i > indent[ilvl] + 3) indent[++ilvl] = i;
+
+					if (separated == 2) *fmt++ = '\n';
+					else if (separated == 1) {
+						/* if ((' ' < *doc && *doc != '-' && *doc < '0') || ('9' < *doc && *doc < 'A') || ('Z' <
+						*doc && *doc < 'a') || 'z' < *doc) { // graph/code text doc -= i; i &= ~1; i -= 2; while
+						(*doc > '\n' || (*doc == '\n' && *++doc != '\n')) { for (int j = i; j && *doc == ' ';
+						j--) doc++; for (int j = lvl * 2 + 6; j; j--) *fmt++ = ' '; while (*doc > '\n') *fmt++ =
+						*doc++; *fmt++ = '\n';
+						  }
+						  doc--;
+						  break;
+						} else */
+						if (optlvl && fmt[-3] != '`' && fmt[-2] == '\n') fmt--;
+					}
+
+					if (ilvl) {
 						int j = 0;
 						if (separated) {
 							const char* tmp = doc;
 							while (*tmp > ' ' || (*tmp > '\n' && (tmp[1] == ' ' || tmp[-1] != ' '))) tmp++;
 							if (*tmp == '\n' && *++tmp != '\n')
 								for (; *tmp == ' '; tmp++, j++) {}
-							if (*tmp == ' ' && tmp - doc == 6) j = -'-';
-							else if (*tmp < 0 || j == i) {
-								if (!separated) doc -= i - indent[0];
-								else doc--;
-								separated = 0;
-								break;
-							} else if (*doc == '-' || (j >= i + 4 && *tmp > '-') || ('A' <= *tmp && *tmp <= 'Z'))
-								j = -'-';
-							else if (*doc >= 'a') {
-								fmt = append(fmt, "```bash\n");
+
+							if (tmp - doc == 6 || *doc == '-' || (j >= i + 4 && *tmp > '-') || ('0' <= *doc && *doc <= '9')) { // align options with their description
+								if (!optlvl) optlvl = ilvl; // ↑ j == indent[ilvl+1] // first option won't appear
+								j = 2 + (i = ilvl * 2 - 1);
+								while (i-- > 0) *fmt++ = ' ';
+								fmt       = append(fmt, *doc == '-' ? "**`" : "- **`");
+								char kind = *doc;
+								while (*doc > ' ' && *doc != '=') *fmt++ = *doc++; // opt name highlight
+								if (kind != '-' && *doc != '=')
+									while (*doc > ' ' || (*doc > '\n' && doc[1] != ' ')) *fmt++ = *doc++;
+								fmt = append(fmt, "`**");
+								if (*doc >= ' ' && doc[1] != ' ') {
+									*fmt++ = *doc++;
+									*fmt++ = '`'; // rest of the option format
+									while (*doc > ' ' || (*doc > '\n' && doc[1] != ' ')) *fmt++ = *doc++;
+									fmt = append(fmt, "`:");
+								} else *fmt++ = ':';
+								if (*doc == '\n' && *++doc > '\n') {
+									*fmt++ = '\n';
+									while (*++doc == ' ') {}
+									while (j-- > 0) *fmt++ = ' ';
+									fmt = append(fmt, "- ");
+								} else {
+									while (*++doc == ' ') {}
+									doc--;
+								}
+							} else if (*doc >= 'a' && *tmp <= '-' && *tmp != ' ' && *tmp > 0) { // example code
+								fmt = append(fmt, "```sh\n");
 								while (*doc > '\n') *fmt++ = *doc++;
 								while (*doc && *++doc > '\n') {
 									*fmt++ = '\n';
@@ -83,46 +116,43 @@ char* bash_fmt(const char* doc, char* fmt, int len) {
 									while (*doc > '\n') *fmt++ = *doc++;
 								}
 								fmt = append(fmt, "```");
-								doc -= 2;
-								break;
-							}
-						} else { // TODO: implement a proper text align system (like lua)
-							i = (i - indent[0] + 1) / 2;
-							while (i-- > 0) *fmt++ = ' ';
-						}
-						if (j == -'-') { // align options with their description
-							kind = '-';
-							j    = 2 + (i = (i - indent[0] + 3) / 2);
-							while (i-- > 0) *fmt++ = ' ';
-							fmt = append(fmt, "- **`");
-							while (*doc > ' ' || (*doc > '\n' && doc[1] != ' ')) *fmt++ = *doc++;
-							fmt = append(fmt, "`**:");
-							if (*doc == '\n' && *++doc > '\n') {
-								*fmt++ = '\n';
-								while (*++doc == ' ') {}
-								while (j-- > 0) *fmt++ = ' ';
-								fmt = append(fmt, "- ");
-							} else {
-								while (*++doc == ' ') {}
 								doc--;
+							} else if (*tmp <= ' ' || j == i) { // normal paragraph
+								for (int i = ilvl * 2 - 1; i; i--) *fmt++ = ' ';
+								if (*tmp > 0) fmt = append(fmt, "- "); // wraptext with utf8 '-'
 							}
-						}
+						} else
+							for (int i = ilvl * 2 + 1; i; i--) *fmt++ = ' ';
 						separated = 0;
+					} else { // lower-level header
+						*fmt++ = '*';
+						*fmt++ = '*';
+						while (*doc > '\n') *fmt++ = *doc++;
+						fmt       = append(fmt, "**");
+						separated = 2;
 					}
+					if (ilvl < optlvl) optlvl = 0;
+					lvl = ilvl;
 					doc--;
 				} else {
 					fmt       = append(fmt, "# ");
 					separated = doc[1];
 					while (*++doc > '\n') *fmt++ = *doc;
-					if (separated == 'S' && fmt[-1] == 'S') { // SYNOPSIS
-						fmt = append(fmt, "\n```bash");
+					if ((separated == 'S' && fmt[-1] == 'S') || (separated == 'N' && fmt[-1] == 'E')) {
+						// SYNOPSIS, NAME
+						if (separated == 'S') fmt = append(fmt, "\n```sh");
+						else *fmt++ = '\n';
 						while (*doc && *++doc > '\n') {
 							*fmt++ = '\n';
-							for (int j = indent[0]; j && *doc == ' '; j--) doc++;
+							for (int i = 7; *doc == ' '; i = 7) {
+								while (i-- && *doc == ' ') doc++;
+								if (i == -1 && *doc == ' ') *fmt++ = ' ';
+							}
 							while (*doc > '\n') *fmt++ = *doc++;
 						}
-						fmt = append(fmt, "```\n");
-					} else separated = -1;
+						if (separated == 'S') fmt = append(fmt, "```\n");
+						else doc--;
+					} else separated = 2;
 					doc--;
 				}
 				break;
@@ -130,12 +160,14 @@ char* bash_fmt(const char* doc, char* fmt, int len) {
 				if (fmt[-1] == -30 && doc[1] == -94) {
 					fmt[-1] = '-';
 					doc++;
+					while (*++doc == ' ') {};
+					*fmt++ = ' ';
+					doc--;
 				} else *fmt++ = -128;
 				break;
 			case '[':
 			case '#':
-			case '*':
-				if (fmt[-1] <= ' ' || doc[1] <= ' ') *fmt++ = '\\';
+			case '*': *fmt++ = '\\';
 			default: *fmt++ = *doc;
 		}
 	}
